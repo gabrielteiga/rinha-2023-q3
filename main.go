db8d52cd1e4235e90ecdacdcade3531a4ec6866f
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-09-18 23:09:32 -0300

    feat: creation of route /ping

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	app.
+		Get("/", func(c *fiber.Ctx) error {
+			return c.SendString("Hello, World!")
+		}).
+		Get("/ping", func(c *fiber.Ctx) error {
+			return c.SendString("pong")
+		})
 
 	app.Listen(":8080")
 }
